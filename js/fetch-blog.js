875c85c22ba0edac9bc9289451ffdad2ba4beff2
SenyuYang <159515401+SenyuYangPDELearner@users.noreply.github.com>
2024-08-09 17:05:49 +0800

    Update fetch-blog.js

--- js/fetch-blog.js
+++ js/fetch-blog.js
@@ -1,9 +1,9 @@
-fetch('https://SenyuYangPDELearner.github.io/template/bloghead.html')
+fetch('../template/bloghead.html')
             .then(response => response.text())
             .then(data => {
                 document.querySelector('#headnav').innerHTML = data;
             });
-fetch('https://SenyuYangPDELearner.github.io/template/foot.html')
+fetch('../template/foot.html')
             .then(response => response.text())
             .then(data => {
                 document.querySelector('#footbar').innerHTML = data;
